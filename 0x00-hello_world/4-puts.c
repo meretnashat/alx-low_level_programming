36b502f4ed1971d7297baa7b6c219778e2216496
meretnashat <n.meret@yahoo.com>
2023-03-16 16:39:09 -0700

    hi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
 * main - Entry point
-*
-* Return: Always 0 (Success)
+* Return: Always 0
 */
 int main(void)
 {
-   Puts("\"Programming is like building a multilingual puzzle");
-   return (0);
+       puts("\"Programming is like building a multilingual puzzle");
+       return (0);
+
 }
